c9b13a16e8ae79b909ab049417f0e885a6685c85
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2023-04-25 18:40:14 +0200

    fnittooooooooooo

--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-const grid100 = document.querySelector(".grid");
-grid100.classList.add('grid-100')
-for (let i = 1; i < 101; i++){
-    grid100.innerHTML+= `<div class="box">${i}</div>`
-    console.log(i)
-}
+const level = document.querySelector("#level");
+const button = document.querySelector(".btn");
+button.addEventListener('click',
+function(){
+    if(level.value == 'easy'){
+        
+        
+        const grid100 = document.querySelector(".grid");
+        grid100.innerHTML= ''
+        grid100.classList.add('grid-100')
+        for (let i = 1; i < 101; i++){
+            grid100.innerHTML+= `<div class="box">${i}</div>`
+            console.log(i)
+        }
+    
+    }
+    else if (level.value == 'normal') {
+        
+        const grid100 = document.querySelector(".grid");
+        grid100.innerHTML= ''
+        grid100.classList.add('grid-81')
+        for (let i = 1; i < 82; i++){
+            grid100.innerHTML+= `<div class="box">${i}</div>`
+            console.log(i)
+        }
+    } else {
+        
+        const grid100 = document.querySelector(".grid");
+        grid100.innerHTML= ''
+        grid100.classList.add('grid-49')
+        for (let i = 1; i < 50; i++){
+            grid100.innerHTML+= `<div class="box">${i}</div>`
+            console.log(i)
+        }
+    }
+
+})
+
